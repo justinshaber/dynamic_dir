818259cdb2435f69f0fbaaf1aae68fa1394e09ff
Justin Shaber <justinshaber@gmail.com>
2024-03-03 13:48:36 -0700

    added link to sort a-z or z-a

--- app.rb
+++ app.rb
@@ -3,7 +3,15 @@ require 'sinatra'
 require 'sinatra/reloader' # causes app to reload its files every time a page is loaded.
 
 get '/' do
-  @file_names = Dir.children("public")
+  @file_names = Dir.children("public").sort
+  @sort = { :path => '/descending', :order => 'Z-A' }
+
+  erb :home
+end
+
+get '/descending' do
+  @file_names = Dir.children("public").sort.reverse
+  @sort = { :path => '/', :order => 'A-Z' }
 
   erb :home
 end
